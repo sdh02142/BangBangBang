a3d8384340c911a07e6bd3c152ac7c8a2b8a76c0
doughnutdough5 <145208718+doughnutdough5@users.noreply.github.com>
2024-11-13 16:28:54 +0900

    fix: 버그 수정

--- src/handler/user/register.handler.js
+++ src/handler/user/register.handler.js
@@ -1,6 +1,22 @@
+import { PACKET_TYPE } from '../../constants/header.js';
+import { GlobalFailCode } from '../../init/loadProtos.js';
+import { createResponse } from '../../utils/response/createResponse.js';
+
 export const registerHandler = (socket, payload) => {
   const { id, password, email } = payload.registerRequest;
   console.log(`id: ${id}, password: ${password}, email: ${email}`);
 
   // 회원가입 처리
+
+  // 응답 생성
+  const responsePayload = {
+    registerResponse: {
+      success: true,
+      message: '회원가입 성공',
+      failCode: GlobalFailCode.NONE_FAILCODE,
+    },
+  };
+
+  // payloadOneofCase, sequence, payload
+  socket.write(createResponse(PACKET_TYPE.REGISTER_RESPONSE, 0, responsePayload));
 };

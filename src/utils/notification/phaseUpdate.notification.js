3f2f95e8b916e8c2e350cc4a4d12c1ae80c369da
doughnutdough5 <145208718+doughnutdough5@users.noreply.github.com>
2024-11-21 11:58:12 +0900

    fix: switch 사용하지 않고 현재 게임의 phase를 통해 맵핑된 시간을 동적으로 사용하게 변경
    Co-authored-by: Freak-J <Freak-J@users.noreply.github.com>

--- src/utils/notification/phaseUpdate.notification.js
+++ src/utils/notification/phaseUpdate.notification.js
@@ -1,24 +1,8 @@
+import phaseTime from '../../constants/phaseTime.js';
 import { characterPositions } from '../../init/loadPositions.js';
 import { Packets } from '../../init/loadProtos.js';
 
 export const phaseUpdateNotification = (game) => {
-  // 밤 30000밀리초
-  // 낮 180000밀리초
-  let nextPhaseAt = 0;
-  switch (game.currentPhase) {
-    case Packets.PhaseType.DAY:
-      nextPhaseAt = 180000;
-      break;
-    // // 음성 채팅만 가능한 회의 시간(붙이려면 클라 추가해야함)
-    // // 지금 어차피 클라에서 안받는다 함
-    // case Packets.PhaseType.EVENING:
-    //   nextPhaseAt = Infinity;
-    //   break;
-    case Packets.PhaseType.END:
-      nextPhaseAt = 30000;
-      break;
-  }
-
   // 낮인 경우만 위치가 다시 셔플돼서 updatePosition
   // 밤에는 현재 위치
   if (game.currentPhase === Packets.PhaseType.DAY) {
@@ -33,7 +17,6 @@ export const phaseUpdateNotification = (game) => {
       const randId = Math.floor(Math.random() * 20);
       selectedPositions.add(characterPositions[randId]); // 0부터 방의 20 길이까지의 랜덤
     }
-    console.log(selectedPositions);
 
     // 선택된 위치 정보는 JSON의 id고, 그걸 접속한 유저의 아이디로 치환
     const posArr = [...selectedPositions];
@@ -47,7 +30,7 @@ export const phaseUpdateNotification = (game) => {
   const responsePayload = {
     phaseUpdateNotification: {
       phaseType: game.currentPhase,
-      nextPhaseAt: Date.now() + nextPhaseAt,
+      nextPhaseAt: Date.now() + phaseTime[game.currentPhase],
       characterPositions: game.users.map((user) => {
         return { id: user.id, x: user.getX(), y: user.getY() };
       }),

e55f5814bf56475e178f457a952df5a17d02eb14
doughnutdough5 <145208718+doughnutdough5@users.noreply.github.com>
2024-11-25 21:23:03 +0900

    update: 낮이 되는 경우 최대 hp를 넘는 카드 랜덤하게 버리는 로직 추가

--- src/utils/notification/phaseUpdate.notification.js
+++ src/utils/notification/phaseUpdate.notification.js
@@ -25,6 +25,19 @@ export const phaseUpdateNotification = (game) => {
       // UPDATE: 초기 좌표 세팅
       inGameUsers[i].updatePosition(posArr[i].x, posArr[i].y);
     }
+
+    // 낮이 시작되면 카드 버려주기(어차피 hp보다 적거나 같으면 안버리면 됨)
+    inGameUsers.forEach((user) => {
+      const userOverHandedCount = user.overHandedCount();
+      if (userOverHandedCount > 0) {
+        for (let i = 0; i < userOverHandedCount; i++) {
+          // 오버한 갯수만큼 랜덤하게 손패 삭제
+          const userDeck = user.characterData.handCards;
+          const randomCard = userDeck[Math.floor(Math.random() * userDeck.length)];
+          user.removeHandCard(randomCard); // 저장될 때 객체로 되면 .type 붙여줘야함
+        }
+      }
+    });
   }
 
   // inGameUsers.forEach((user) => {

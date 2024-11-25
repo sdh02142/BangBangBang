d139e21d9f39c83204262a285d0166fa8dfab556
sdh02142 <sdh02142@naver.com>
2024-11-25 17:37:05 +0900

    update: 카드 사용 user 검증 if문 추가

--- src/handler/card/shieldCard.handler.js
+++ src/handler/card/shieldCard.handler.js
@@ -1,16 +1,19 @@
 import { getStateNormal } from '../../constants/stateType.js';
-import { animationNotification } from '../../utils/notification/animation.notification.js';
 import { Packets } from '../../init/loadProtos.js';
-
 
 export const shieldCardHandler = (cardUsingUser, targetUser, currentGame) => {
   console.log('쉴드 쓴 사람:', cardUsingUser.id);
-  // 예약된 이벤트 삭제가 제대로 되지 않음
-  // currentGame.removeEvent(cardUsingUser.id);
-  console.log(currentGame.events.cancelEvent);
-  currentGame.events.cancelEvent(cardUsingUser.id, 'finishShieldWait');
-  currentGame.events.cancelEvent(cardUsingUser.id, 'finishShieldWaitOnBigBbang');
+  
+  if (cardUsingUser.characterData.stateInfo.state === Packets.CharacterStateType.BBANG_TARGET)
+  {
+    currentGame.events.cancelEvent(cardUsingUser.id, 'finishShieldWait');
+  }
 
+  if (cardUsingUser.characterData.stateInfo.state === Packets.CharacterStateType.BIG_BBANG_TARGET)
+  {
+    currentGame.events.cancelEvent(cardUsingUser.id, 'finishShieldWaitOnBigBbang');
+  }
+    
   cardUsingUser.setCharacterState(getStateNormal());
   targetUser.setCharacterState(getStateNormal());
 };

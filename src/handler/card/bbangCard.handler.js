c1a883cf8b366ff0a6b748440b67e14c6f658032
전지훈 <jihun1162@gmail.com>
2024-11-26 12:11:18 +0900

    fix: 빵야 실패 로직 주석처리 / 나중에 논의할 사항

--- src/handler/card/bbangCard.handler.js
+++ src/handler/card/bbangCard.handler.js
@@ -9,10 +9,14 @@ import { Packets } from '../../init/loadProtos.js';
 import userUpdateNotification from '../../utils/notification/userUpdate.notification.js';
 
 export const bbangCardHandler = (cardUsingUser, targetUser, currentGame) => {
-
-  if (cardUsingUser.characterData.stateInfo.state === Packets.CharacterStateType.NONE_CHARACTER_STATE) {
+  if (
+    cardUsingUser.characterData.stateInfo.state === Packets.CharacterStateType.NONE_CHARACTER_STATE
+  ) {
     normalBbangHandler(cardUsingUser, targetUser, currentGame);
-  } else if (cardUsingUser.characterData.stateInfo.state === Packets.CharacterStateType.DEATH_MATCH_TURN_STATE) {
+  } else if (
+    cardUsingUser.characterData.stateInfo.state ===
+    Packets.CharacterStateType.DEATH_MATCH_TURN_STATE
+  ) {
     deathMatchBbangHandler(cardUsingUser, targetUser, currentGame);
   } else if (
     cardUsingUser.characterData.stateInfo.state === Packets.CharacterStateType.GUERRILLA_TARGET
@@ -29,26 +33,32 @@ const guerrillaBbangHandler = (cardUsingUser, targetUser, currentGame) => {
 
 const deathMatchBbangHandler = (cardUsingUser, targetUser, currentGame) => {
   currentGame.events.cancelEvent(cardUsingUser.id, 'onDeathMatch');
-  currentGame.events.scheduleEvent(targetUser.id, 'onDeathMatch', 5000, { cardUsingUser, targetUser, stateNormal: getStateNormal(), userUpdateNotification, currentGameUsers: currentGame.users})
+  currentGame.events.scheduleEvent(targetUser.id, 'onDeathMatch', 5000, {
+    cardUsingUser,
+    targetUser,
+    stateNormal: getStateNormal(),
+    userUpdateNotification,
+    currentGameUsers: currentGame.users,
+  });
 
   // 시전자 state 변경(빵야 카드 사용 시: 현피 기다림)
   cardUsingUser.setCharacterState(getStateDeathInitShooter(targetUser.id));
   // 대상자 state 변경(현피 대상: 빵야 카드 소지 여부 및 사용 여부)
   targetUser.setCharacterState(getStateDeathInitTarget(cardUsingUser.id));
-}
+};
 
 const normalBbangHandler = (cardUsingUser, targetUser, currentGame) => {
   const currentGameUsers = currentGame.users;
-  if (cardUsingUser.canUseBbang()) {
-    // 빵야 실패
-    const errorResponse = {
-      useCardResponse: {
-        success: false,
-        failCode: Packets.GlobalFailCode.ALREADY_USED_BBANG,
-      },
-    };
-    return errorResponse;
-  }
+  // if (!cardUsingUser.canUseBbang()) { // 이 로직을 여기에 둬야할지 생각해봐야함 빵야 카드는 사용이 되고 빵야가 안됨 카드 사용이 안되야 함
+  //   // 빵야 실패
+  //   const errorResponse = {
+  //     useCardResponse: {
+  //       success: false,
+  //       failCode: Packets.GlobalFailCode.ALREADY_USED_BBANG,
+  //     },
+  //   };
+  //   return errorResponse;
+  // }
 
   // 여기서부터 빵야 사용 로직
   // 빵야 카운트 증가

60a02de256df4b536fe44d82c21151afaddbc3c4
doughnutdough5 <145208718+doughnutdough5@users.noreply.github.com>
2024-11-26 21:02:38 +0900

    fix: 개굴군 로직 수정
    Co-authored-by: LEE, DONGEUN <armsterd@users.noreply.github.com>

--- src/handler/character/froggy.handler.js
+++ src/handler/character/froggy.handler.js
@@ -4,5 +4,6 @@ import { animationNotification } from '../../utils/notification/animation.notifi
 export const froggyHandler = (user, game) => {
   // 개굴군 로직 추가
   // 25% 확률로 공격 막기 -> 애니메이션 노티 날려야함
-  animationNotification(game.users, user, Packets.AnimationType.SHIELD_ANIMATION);
+  // animationNotification(game.users, user, Packets.AnimationType.SHIELD_ANIMATION); // 튜터님 질문 : 쉴드 애니메이션이 자동쉴드 때만 발동인데, 개굴군의 25% 확률도 자동쉴드의 범주안에 들어가는지
+  game.events.cancelEvent(user.id, 'finishShieldWait');
 };

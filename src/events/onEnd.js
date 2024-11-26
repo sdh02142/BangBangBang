e6df2b656c0de91c2874b43e7f8e309bc4438e23
bigsmile1460 <wns4711@naver.com>
2024-11-26 16:20:35 +0900

    update: 연결이 끊기면 인게임 유저 사망 처리 추가

--- src/events/onEnd.js
+++ src/events/onEnd.js
@@ -1,6 +1,12 @@
-import { removeUser } from '../sessions/user.session.js';
+import { findGameById, getAllGameSessions } from '../sessions/game.session.js';
+import { getUserBySocket, removeUser } from '../sessions/user.session.js';
 
 export const onEnd = (socket) => () => {
   console.log('클라이언트 연결이 종료되었습니다.');
+  const user = getUserBySocket(socket);
+  if (user.roomId){ //유저가 게임을 진행중인 상황
+    user.setHp(0)
+  }
+  
   removeUser(socket);
 };

ffe9babd45b22ee39175d3a285aa7829210da7bd
doughnutdough5 <145208718+doughnutdough5@users.noreply.github.com>
2024-11-26 13:09:54 +0900

    fix: 카드 삭제 핸들러 완성. 동기화 코드를 추가하고, removeHandCard 호출할 때 보내주던 매개변수 수정하였음.

--- src/handler/card/destroyCard.handler.js
+++ src/handler/card/destroyCard.handler.js
@@ -1,4 +1,5 @@
 import { PACKET_TYPE } from '../../constants/header.js';
+import { findGameById } from '../../sessions/game.session.js';
 import { getUserBySocket } from '../../sessions/user.session.js';
 import userUpdateNotification from '../../utils/notification/userUpdate.notification.js';
 import { createResponse } from '../../utils/response/createResponse.js';
@@ -10,29 +11,41 @@ import { createResponse } from '../../utils/response/createResponse.js';
 // 페이로드: {"destroyCardRequest":{"destroyCards":}}
 // phaseUpdate에서 한번더 삭제 로직을 넣으면? <-- 어차피 이 리퀘가 날라오면 피보다 카드수가 적을 것이기 때문에, 노티에서 hp에 따라 랜덤으로 카드를 삭제해주면 로직 겹칠일은 없음
 export const destroyCardHandler = (socket, payload) => {
-  const destroyCards = payload.destroyCardRequest.flatMap(({ type, count }) =>
+  console.log('카드 삭제 request 날라옴');
+  const destroyCards = payload.destroyCardRequest.destroyCards.flatMap(({ type, count }) =>
     new Array(count).fill(type),
-  ); // 객체 가진 배열 -> [{"type":"BBANG","count":1}]
-  console.log(destroyCards);
+  );
+  console.log('flat화 된 카드 뭉치', destroyCards);
   const cardDestroyUser = getUserBySocket(socket);
 
+  console.log('삭제 전 손패', cardDestroyUser.characterData.handCards);
+
   destroyCards.forEach((card) => {
-    cardDestroyUser.removeHandCard(card.type);
+    cardDestroyUser.removeHandCard(card);
   });
 
   // 동기화 해줘야하나?
+  const currentGame = findGameById(cardDestroyUser.roomId);
+  userUpdateNotification(currentGame.users);
 
   // response
   const responsePayload = {
     destroyCardResponse: {
-      // 삭제 된 손패?
+      handCards: cardDestroyUser.characterData.handCards.map((card) => {
+        return { type: card.type, count: card.count };
+      }),
     },
   };
 
   socket.write(createResponse(PACKET_TYPE.DESTROY_CARD_RESPONSE, 0, responsePayload));
 
-  console.log('카드 삭제 request 날라옴');
+  console.log('삭제 후 손패', cardDestroyUser.characterData.handCards);
 };
+
+// message CardData {
+//     CardType type = 1;
+//     int32 count = 2;
+// }
 
 // message C2SDestroyCardRequest {
 //     repeated CardData destroyCards = 1;

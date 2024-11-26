4a6463c8cb77fd11136bffc885dca7cb0b770dfd
전지훈 <jihun1162@gmail.com>
2024-11-26 10:38:40 +0900

    update: 무기를 장착하기전에 무기가 있다면 무기를 장착하기 전 상태로 되돌리고 해당 무기 장착

--- src/handler/card/desertEagleCard.handler.js
+++ src/handler/card/desertEagleCard.handler.js
@@ -1,5 +1,6 @@
 import { Packets } from '../../init/loadProtos.js';
 
 export const desertEagleCardHandler = (cardUsingUser, targetUser, currentGame) => {
+  if (cardUsingUser.characterData.weapon !== 0) cardUsingUser.unequipWepon();
   cardUsingUser.equipWepon(Packets.CardType.DESERT_EAGLE);
 };
--- src/handler/card/handGunCard.handler.js
+++ src/handler/card/handGunCard.handler.js
@@ -1,5 +1,6 @@
 import { Packets } from '../../init/loadProtos.js';
 
 export const handGunCardHandler = (cardUsingUser, targetUser, currentGame) => {
+  if (cardUsingUser.characterData.weapon !== 0) cardUsingUser.unequipWepon();
   cardUsingUser.equipWepon(Packets.CardType.HAND_GUN);
 };

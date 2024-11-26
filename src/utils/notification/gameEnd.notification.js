eb88a0763470bf7151179153133a66d3dc5f2aad
bigsmile1460 <wns4711@naver.com>
2024-11-26 18:13:33 +0900

    fix: 게임 승리 중 싸이코패스와 히트맨 우선 순위 변경

--- src/utils/notification/gameEnd.notification.js
+++ src/utils/notification/gameEnd.notification.js
@@ -5,7 +5,6 @@ import { removeGameSession } from '../../sessions/game.session.js';
 import { roomManager } from '../../classes/manager/room.manager.js';
 
 export const gameEndNotification = (room) => { 
-  // 정상적으로 게임이 끝나는 경우
   const survivor = [];
   room.users.forEach((user) => {
     if (user.characterData.hp > 0) {
@@ -21,21 +20,9 @@ export const gameEndNotification = (room) => {
 
   let responsePayload;
 
-  if (!isTarget && isHitman) {
-    //히트맨 승리
-    console.log('히트맨 승리');
-    const winners = room.users.filter((user) => user.characterData.roleType === Packets.RoleType.HITMAN);
-    const winnerIds = winners.map((user) => user.id);
-    responsePayload = {
-      gameEndNotification: {
-        winners: winnerIds,
-        winType: Packets.WinType.HITMAN_WIN,
-      },
-    };
-  }
+
   if (!isTarget && !isBodyguard && !isHitman && isPsychopath) {
     //싸이코패스 승리
-    console.log('싸이코패스 승리');
     const winners = room.users.filter((user) => user.characterData.roleType === Packets.RoleType.PSYCHOPATH);
     const winnerIds = winners.map((user) => user.id);
     responsePayload = {
@@ -45,9 +32,19 @@ export const gameEndNotification = (room) => {
       },
     };
   }
-  if (!isHitman && !isPsychopath) {
+  if (!isTarget && isHitman) {
+    //히트맨 승리
+    const winners = room.users.filter((user) => user.characterData.roleType === Packets.RoleType.HITMAN);
+    const winnerIds = winners.map((user) => user.id);
+    responsePayload = {
+      gameEndNotification: {
+        winners: winnerIds,
+        winType: Packets.WinType.HITMAN_WIN,
+      },
+    };
+  }
+  if (!isHitman && !isPsychopath && isTarget) {
     //타겟 & 보디가드 승리
-    console.log('타겟&보디가드 승리');
     const winners = room.users.filter((user) => user.characterData.roleType === Packets.RoleType.TARGET || user.characterData.roleType === Packets.RoleType.BODYGUARD);
     const winnerIds = winners.map((user) => user.id);
     responsePayload = {
@@ -76,7 +73,7 @@ export const gameEndNotification = (room) => {
 // }
 
 // enum WinType {
-//     TARGET_AND_BODYGUARD_WIN = 0; 보안관 + 부관
-//     HITMAN_WIN = 1; 무법자
-//     PSYCHOPATH_WIN = 2; 배신자
+//     TARGET_AND_BODYGUARD_WIN = 0;
+//     HITMAN_WIN = 1;
+//     PSYCHOPATH_WIN = 2;
 // }

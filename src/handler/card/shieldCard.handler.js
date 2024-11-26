947876d530183f1d1985bda599eb49e1eb49c242
doughnutdough5 <145208718+doughnutdough5@users.noreply.github.com>
2024-11-26 21:01:36 +0900

    fix: 상어군 관련 로직 수정
    Co-authored-by: sdh02142 <sdh02142@users.noreply.github.com>
    Co-authored-by: LEE, DONGEUN <armsterd@users.noreply.github.com>
    Co-authored-by: Freak-J <Freak-J@users.noreply.github.com>
    Co-authored-by: Park Junho <bigsmile1460@users.noreply.github.com>

--- src/handler/card/shieldCard.handler.js
+++ src/handler/card/shieldCard.handler.js
@@ -1,16 +1,17 @@
 import { getStateNormal } from '../../constants/stateType.js';
 import { Packets } from '../../init/loadProtos.js';
 
-export const shieldCardHandler = (cardUsingUser, targetUser, currentGame) => {
+export const shieldCardHandler = (cardUsingUser, targetUser, currentGame, useCardType) => {
   console.log('쉴드 쓴 사람:', cardUsingUser.id);
 
   if (cardUsingUser.characterData.stateInfo.state === Packets.CharacterStateType.BBANG_TARGET) {
-    if (cardUsingUser.characterData.characterType == Packets.CharacterType.SHARK) {
-      // 쉴드 카드 사용 request가 두번 날라오는 걸 체크할 수 있나??
-      // 스택을 두번 쌓아야 cancelEvent되게끔
-    } else {
-      currentGame.events.cancelEvent(cardUsingUser.id, 'finishShieldWait');
-    }
+    if (cardUsingUser.characterData.characterType === Packets.CharacterType.SHARK) {
+      // 상어군이 빵야를 맞았을 때 쉴드를 사용하면 일차적으로 1개씩 까고, useCard.handler에서 한 번 더 깜 -> 최종적으로 빵야카드를 막기위해 2개의 쉴드 사용
+      // cardUsingUser.decreaseHandCardsCount(); // 카드 사용자의 손에 들고 있던 카드 수 감소
+      cardUsingUser.removeHandCard(useCardType); // 카드 사용자의 손에 들고 있던 카드 제거
+      currentGame.returnCardToDeck(useCardType); // 카드 덱으로 복귀
+    } 
+    currentGame.events.cancelEvent(cardUsingUser.id, 'finishShieldWait');
   }
 
   if (cardUsingUser.characterData.stateInfo.state === Packets.CharacterStateType.BIG_BBANG_TARGET) {
